2b8458097dcf0b26ae925adf21d61ae09464e3fb
Anatoliy <vas11yev@yandex.ru>
2019-01-03 02:02:49 +0500

    lang-filters

--- app/js/langs.js
+++ app/js/langs.js
@@ -1,10 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
 	let lang = document.querySelectorAll('.languages-button');
-
 	lang.forEach(function(el, i) {
 		el.addEventListener('click', () => {
-			lang[0].classList.toggle('active');
-			lang[1].classList.toggle('active');
+			if(i == 0){
+				lang[0].classList.add('active')
+				lang[1].classList.remove('active')
+			}else{
+				lang[0].classList.remove('active')
+				lang[1].classList.add('active')
+			}
 		});
 	});
 });
