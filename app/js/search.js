54c94dbbdace3f8cfd54ff3ea354ffe03a88ff31
Anatoliy <vas11yev@yandex.ru>
2019-01-04 00:12:34 +0500

    final mobile

--- app/js/search.js
+++ app/js/search.js
@@ -1,11 +1,4 @@
 window.addEventListener('DOMContentLoaded', () => {
-	let searchBar = document.querySelector('.search-bar');
-	let searchString = document.querySelector('.search-string');
-	let alphaString = document.querySelector('.alpha-string');
-	let langButton = document.querySelectorAll('.languages-button');
-
-    let i = 0;
-
 
 	let openButton = document.querySelector('.open-button');
 	let menu = document.querySelector('.main-menu');
